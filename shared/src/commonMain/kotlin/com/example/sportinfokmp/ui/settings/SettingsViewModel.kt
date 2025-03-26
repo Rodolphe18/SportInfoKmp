06e2d086bb3eff7442a579dc606ba47b1f551b91
rodolphe <rodolphefrancotte18@gmail.com>
2025-03-26 12:42:59 +0100

    settings VM implemented

---
+++ shared/src/commonMain/kotlin/com/example/sportinfokmp/ui/settings/SettingsViewModel.kt
@@ -0,0 +1,58 @@
+package com.example.sportinfokmp.ui.settings
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.sportinfokmp.data.datastore.DarkThemeConfig
+import com.example.sportinfokmp.data.datastore.ThemeBrand
+
+import com.example.sportinfokmp.data.datastore.UserDataRepository
+import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.seconds
+
+
+class SettingsViewModel (
+    private val userDataRepository: UserDataRepository,
+) : ViewModel() {
+    val settingsUiState: StateFlow<SettingsUiState> =
+        userDataRepository.userData
+            .map { userData ->
+                SettingsUiState.Success(
+                    settings = UserEditableSettings(
+                        brand = userData?.themeBrand ?: ThemeBrand.ANDROID,
+                        darkThemeConfig = userData?.darkThemeConfig ?: DarkThemeConfig.LIGHT,
+                    ),
+                )
+            }
+            .stateIn(
+                scope = viewModelScope,
+                started = WhileSubscribed(5.seconds.inWholeMilliseconds),
+                initialValue = SettingsUiState.Loading,
+            )
+
+    fun updateThemeBrand(themeBrand: ThemeBrand) {
+        viewModelScope.launch {
+            userDataRepository.setThemeBrand(themeBrand)
+        }
+    }
+
+    fun updateDarkThemeConfig(darkThemeConfig: DarkThemeConfig) {
+        viewModelScope.launch {
+            userDataRepository.setDarkThemeConfig(darkThemeConfig)
+        }
+    }
+
+}
+
+data class UserEditableSettings(
+    val brand: ThemeBrand,
+    val darkThemeConfig: DarkThemeConfig,
+)
+
+sealed interface SettingsUiState {
+    data object Loading : SettingsUiState
+    data class Success(val settings: UserEditableSettings) : SettingsUiState
+}
