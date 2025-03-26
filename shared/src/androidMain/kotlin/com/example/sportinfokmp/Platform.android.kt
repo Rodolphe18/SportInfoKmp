912b48a830111a7ec0005270e1476b5339ff1c58
rodolphe <rodolphefrancotte18@gmail.com>
2025-03-26 12:13:25 +0100

    useless files deleted

--- shared/src/androidMain/kotlin/com/example/sportinfokmp/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package com.example.sportinfokmp
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
